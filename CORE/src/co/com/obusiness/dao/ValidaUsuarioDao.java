58a82c6d6707741983c551829e736360d87c6aa3
hossa2016 <67599457+hossa2016@users.noreply.github.com>
2020-06-29 12:23:50 -0500

    Delete ValidaUsuarioDao.java

--- CORE/src/co/com/obusiness/dao/ValidaUsuarioDao.java
+++
@@ -1,8 +0,0 @@
-package co.com.obusiness.dao;
-
-import co.com.obusiness.entidades.TsEmpUsuarios;
-
-public interface ValidaUsuarioDao {
-	
-	public String validaUsuario(TsEmpUsuarios usuario) throws Exception;
-}
